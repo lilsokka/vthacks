c02c31eefe78d92be33a30a5b25d512188d821f5
bennett-gg <145243573+bennett-gg@users.noreply.github.com>
2023-09-17 07:21:02 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,27 +1,67 @@
-const code = [
-    `import java.util.Scanner;
-    public class FactorialCalculator {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter a non-negative integer: ");
-            int n = scanner.nextInt();
-    
-            if (n < 0) {
-                System.out.println("Factorial is undefined for negative numbers.");
-            } else {
-                long factorial = calculateFactorial(n);
-                System.out.println("Factorial of " + n + " is " + factorial);
-            }
-    
-            scanner.close();
-        }
-    
-        public static long calculateFactorial(int n) {
-            if (n == 0 || n == 1) {
-                return 1;
-            } else {
-                return n * calculateFactorial(n - 1);
-            }
-        }
-    }`
+printSnippetOne = [
+    'import java.util.Scanner;', 
+    '    public class FactorialCalculator {',
+    '        public static void main(String[] args) {',
+    '            Scanner input = new Scanner(System.in);',
+    '        System.out.print("Enter a non-negative integer: ");',
+    '        int n = input.nextInt();',
+    '',
+    '       if (n < 0) {',
+    '            System.out.println("Factorial is undefined for negative numbers.");',
+    '        } else {',
+    '            long factorial = calculateFactorial(n);',
+    '            System.out.println("Factorial of " + n + " is " + factorial);',
+    '        }',
+    '',
+    '        input.close();',
+    '    }',
+    '',
+    '    public static long calculateFactorial(int n) {',
+    '        if (n == 0 || n == 1) {',
+    '            return 1;',
+    '        } else {',
+    '            return n * calculateFactorial(n - 1);',
+    '        }',
+    '    }',
+    '}'
+];
+typeSnippetOne = [
+    'import java.util.Scanner;', 
+    'public class FactorialCalculator {',
+    'public static void main(String[] args) {',
+    'Scanner input = new Scanner(System.in);',
+    'System.out.print("Enter a non-negative integer: ");',
+    'int n = input.nextInt();',
+    'if (n < 0) {',
+    'System.out.println("Factorial is undefined for negative numbers.");',
+    '} else {',
+    'long factorial = calculateFactorial(n);',
+    'System.out.println("Factorial of " + n + " is " + factorial);',
+    '}',
+    'input.close();',
+    '}',
+    'public static long calculateFactorial(int n) {',
+    'if (n == 0 || n == 1) {',
+    'return 1;',
+    '} else {',
+    'return n * calculateFactorial(n - 1);',
+    '}',
+    '}',
+    '}'
+];
+typeSnippetTwo = [
+    'this is just a test',
+    'testing 1',
+    'testing 2',
+    '1234',
+    '56789',
+    'testing 5',
+    'testing 6',
+    'testing 7',
+]
+const typeSnippets = [
+    typeSnippetOne , typeSnippetTwo
+];
+const codeSnippets = [
+    printSnippetOne
 ];
--- main.js
+++ main.js
@@ -7,27 +7,58 @@ wpmTag = document.querySelector(".wpm span"),
 cpmTag = document.querySelector(".cpm span");
 
 let timer,
-maxTime = 60,
+maxTime = 0,
 timeLeft = maxTime,
-charIndex = mistakes = isTyping = 0;
+charIndex = mistakes = isTyping = typedCount = totalTyped = oldChars = 0;
 
+function loadCode(currentLine = 0, currentTest = -1, totalLines = 0) {
+    typingText.innerHTML = null
+    charIndex = 0
+    
+    if (currentTest == -1)  {
+        currentTest = Math.floor(Math.random() * typeSnippets.length);
+        totalLines = typeSnippets[currentTest].length;
+    }
+    i = currentLine
 
-function loadCode() {
-    const ranIndex = Math.floor(Math.random() * code.length);
-    typingText.innerHTML = "",
-    code[ranIndex].split("").forEach(char => {
-        let span = `<span>${char}</span>`
-        typingText.innerHTML += span;
-    });
+    if (i != 0) {
+        typedCount += 1;
+
+    }
+        
+    if (typeSnippets[currentTest].length < currentLine + 4){
+        for  (i; i < typeSnippets[currentTest].length; i++) {
+            typeSnippets[currentTest][i].split("").forEach(char => {
+                let span = `<span>${char}</span>`
+                typingText.innerHTML += span;
+            });
+            typingText.innerHTML += '<br></br>'
+            currentLine++;
+        }
+    } else {
+        for(i; i < currentLine + 4; i++)  {
+            typeSnippets[currentTest][i].split("").forEach(char => {
+                let span = `<span>${char}</span>`
+                typingText.innerHTML += span;
+            });
+            typingText.innerHTML += '<br></br>'
+        }
+        currentLine += 4;
+    }
     typingText.querySelectorAll("span")[0].classList.add("active");
     document.addEventListener("keydown", () => inpField.focus());
     typingText.addEventListener("click", () => inpField.focus());
+    
+    return [currentLine, currentTest, totalLines];
+
 }
+
+
 
 function initTyping() {
     let characters = typingText.querySelectorAll("span");
-    let typedChar = inpField.value.split("")[charIndex];
-    if(charIndex < characters.length - 1 && timeLeft > 0) {
+    let typedChar = inpField.value.split("")[typedCount];
+    if(charIndex < characters.length - 1) {
         if(!isTyping) {
             timer = setInterval(initTimer, 1000);
             isTyping = true;
@@ -35,51 +66,81 @@ function initTyping() {
         if(typedChar == null) {
             if(charIndex > 0) {
                 charIndex--;
-                if(characters[charIndex].classList.contains("incorrect")) {
-                    mistakes--;
+                typedCount--;
+                try{
+                    if(characters[charIndex].classList.contains("incorrect")) {
+                        mistakes--;
+                    }
+                    characters[charIndex].classList.remove("correct", "incorrect");
+                } catch (err)   {
+                    window.alert("Cannot go back:", charIndex, ' ', typedCount)
                 }
-                characters[charIndex].classList.remove("correct", "incorrect");
+            } else {
+                exception = true
             }
         } else {
-            if(characters[charIndex].innerText == typedChar) {
-                characters[charIndex].classList.add("correct");
-            } else {
-                mistakes++;
-                characters[charIndex].classList.add("incorrect");
+            try{
+                if(characters[charIndex].innerText == typedChar) {
+                    characters[charIndex].classList.add("correct");
+                } else {
+                    mistakes++;
+                    characters[charIndex].classList.add("incorrect");
+                }
+                charIndex++;
+                typedCount++;
+            } catch (err)   {
+                window.alert("Cannot go back:", charIndex, ' ', typedCount)
             }
-            charIndex++;
+            
         }
+    
         characters.forEach(span => span.classList.remove("active"));
         characters[charIndex].classList.add("active");
 
-        let wpm = Math.round(((charIndex - mistakes)  / 5) / (maxTime - timeLeft) * 60);
+        let wpm = Math.round(((typedCount - mistakes)  / 5) / ((timeLeft * 60)/100));
         wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
         
         wpmTag.innerText = wpm;
         mistakeTag.innerText = mistakes;
-        cpmTag.innerText = charIndex - mistakes;
+        cpmTag.innerText = typedCount - mistakes;
+
+    } else if (linesList[0] < linesList[2]) {
+        typingText.innerHTML = ""
+        linesList = loadCode(linesList[0], linesList[1], linesList[2]);
+        charIndex = 0
+        mistakeTag.innerText = characters[charIndex].innerText
+        wpmTag.innerText = typedChar
+        cpmTag.innerText = charIndex
+        timeTag.innerText = typedCount
+
     } else {
+        infoForDone();
         clearInterval(timer);
         inpField.value = "";
-    }   
+    }
+     
 }
 
+
+
 function initTimer() {
-    if(timeLeft > 0) {
-        timeLeft--;
+    if(isTyping) {
+        timeLeft++;
         timeTag.innerText = timeLeft;
-        let wpm = Math.round(((charIndex - mistakes)  / 5) / (maxTime - timeLeft) * 60);
+        let wpm = Math.round(((typedCount - mistakes)  / 5) / ((timeLeft * 60)/100));
         wpmTag.innerText = wpm;
     } else {
         clearInterval(timer);
     }
 }
 
+
+
 function resetGame() {
-    loadParagraph();
+    linesList = loadCode();
     clearInterval(timer);
     timeLeft = maxTime;
-    charIndex = mistakes = isTyping = 0;
+    charIndex = mistakes = isTyping = typedCount = 0;
     inpField.value = "";
     timeTag.innerText = timeLeft;
     wpmTag.innerText = 0;
@@ -87,6 +148,12 @@ function resetGame() {
     cpmTag.innerText = 0;
 }
 
-loadCode();
+function infoForDone()  {
+
+}
+
+
+timeTag.innerText = timeLeft;
+linesList = loadCode();
 inpField.addEventListener("input", initTyping);
 tryAgainBtn.addEventListener("click", resetGame);
